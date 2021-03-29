4cdfcb7c748b7ff1a9b8a29df8cf5be7ff04ead0
Hebitz Lau <hebitz.lau@qbssystem.com>
2021-03-29 21:09:29 +0800

    Fixed keypress event

--- control-panel/ui/src/components/CarControl.vue
+++ control-panel/ui/src/components/CarControl.vue
@@ -2,41 +2,46 @@
   <div>
     <h1>Hi</h1>
     <Keypress
-      key-event="keypress"
-      :key-code="65"
-      @success="directionKeyHandler"
-    />
-    <Keypress
-      key-event="keypress"
-      :key-code="87"
-      @success="directionKeyHandler"
-    />
-    <Keypress
-      key-event="keypress"
-      :key-code="68"
-      @success="directionKeyHandler"
-    />
-    <Keypress key-event="keypress" :key-code="83" @success="someMethod" />
-    <!-- <Keypress
-      key-event="keypress"
+      key-event="keydown"
       :key-code="38"
       @success="directionKeyHandler(38)"
     />
     <Keypress
-      key-event="keypress"
+      key-event="keydown"
+      :key-code="40"
+      @success="directionKeyHandler(40)"
+    />
+    <Keypress
+      key-event="keydown"
+      :key-code="37"
+      @success="directionKeyHandler(37)"
+    />
+    <Keypress
+      key-event="keydown"
       :key-code="39"
       @success="directionKeyHandler(39)"
     />
-    <Keypress
-      key-event="keypress"
-      :key-code="40"
-      @success="directionKeyHandler(40)"
-    /> -->
-    <Keypress key-event="keypress" :key-code="13" @success="someMethod" />
+    <Keypress key-event="keyup" :key-code="38" @success="stopTheCar" />
+    <Keypress key-event="keyup" :key-code="40" @success="stopTheCar" />
+    <Keypress key-event="keyup" :key-code="37" @success="stopTheCar" />
+    <Keypress key-event="keyup" :key-code="39" @success="stopTheCar" />
+
+    <Keypress key-event="keydown" :key-code="32" @success="captureImg" />
+    <Keypress key-event="keydown" :key-code="84" @success="toggleDrivingMode" />
   </div>
 </template>
 
 <script>
+// const socket = new WebSocket("ws://158.132.55.98:81");
+const socket = new WebSocket("ws://158.132.55.92:82/client");
+socket.addEventListener("open", function () {
+  socket.send("Hello Server!");
+});
+
+// Listen for messages
+socket.addEventListener("message", function (event) {
+  console.log("Message from server ", event.data);
+});
 export default {
   name: "CarControlComp",
   components: {
@@ -45,30 +50,42 @@ export default {
   data() {
     return {
       isConnected: false,
+      isManual: true,
     };
   },
+  mounted() {},
   methods: {
-    someMethod(event, expectedEvent, message) {
+    captureImg() {
       // Do something
-      console.log("sth wrong");
-      console.log(event, expectedEvent, message);
+      console.log("Image captured");
     },
-    directionKeyHandler(event) {
-      console.log(event);
-      //   switch (keyCode) {
-      //     case 37: // Arrow LEFT
-      //       console.log("left");
-      //       break;
-      //     case 38: // Arrow UP
-      //       console.log("up");
-      //       break;
-      //     case 39: // Arrow RIGHT
-      //       console.log("right");
-      //       break;
-      //     case 40: // Arrow DOWN
-      //       console.log("down");
-      //       break;
-      //   }
+    toggleDrivingMode() {
+      console.log("hi");
+      this.isManual = !this.isManual;
+    },
+    stopTheCar() {
+      socket.send(`STOP`); //0
+    },
+    directionKeyHandler(keyCode) {
+      switch (keyCode) {
+        case 37: // Arrow LEFT
+          console.log("left"); //4
+          socket.send(`LEFT`);
+          break;
+        case 38: // Arrow UP
+          console.log("up"); //1
+          socket.send(`UP`);
+
+          break;
+        case 39: // Arrow RIGHT
+          console.log("right"); //3
+          socket.send(`RIGHT`);
+          break;
+        case 40: // Arrow DOWN
+          console.log("back"); //2
+          socket.send(`BACK`);
+          break;
+      }
     },
   },
 };
